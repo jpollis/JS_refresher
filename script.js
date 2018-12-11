f2cb29900a8fe995167ea84d8dd9bde8647e04a1
Joe Willy <34250239+jpollis@users.noreply.github.com>
2018-12-11 10:11:20 -0500

    objects

--- script.js
+++ script.js
@@ -421,13 +421,33 @@ return isJob;
 console.log(checkJobIndex(joe, 'teacher'));
 
 
+/*********************************************
+* Objects and Properties
+*/
 
+var john = {
+	firstName: "John",
+	lastName: "Smith",
+	birthYear: 1990,
+	family: ['Jane', 'Mark', 'Bob', 'Emily'],
+	job: 'teacher',
+	isMarried: false
+};
 
+console.log(john.firstName);
+console.log(john['lastName']);
+var x = 'birthYear';
+console.log(john[x]);
 
+john.job = 'designer';
+john['isMarried'] = true;
+console.log(john);
 
-
-
-
+var jane = new Object();
+jane.name = 'Jane';
+jane.birthYear = 1969;
+jane['lastName'] = 'Smith';
+console.log(jane);
 
 
 
